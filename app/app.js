1d8a5ce90511ca86a2b048e397f8d143f3f89cd8
rudi <rudi.setiyadi@mu.co.id>
2025-11-12 15:21:39 +0700

    update js

--- app/app.js
+++ app/app.js
@@ -10,7 +10,6 @@ app.use(logger)
 const connection = require('./middleware/db_connect');
 
 app.disable("x-powered-by");
-
 // Dashboard
 app.use('/', express.static(path.join(__dirname, 'public')));
 
